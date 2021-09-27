c282e1455acd547828ac8d25099df281c52cfc77
∧° ┐ <we.are.pe.hu@gmail.com>
2021-09-28 07:09:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ body {margin:0; padding:0;}
 #you img {width: 7.5vw;}
 
 @media print{
-#greeting, #btn, #menu {display:none;}
+#greeting, #index {display:none;}
 #you,
 #submit {display:block;}
 #submit {
