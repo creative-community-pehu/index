052aec886316bcfc1cec40275f7f6c77bcf74216
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-08 05:20:18 +0900

    Update new.php

--- new.php
+++ new.php
@@ -29,98 +29,12 @@ body {margin:0; padding:0;}
 #ver #grid div:nth-child(n + 7) {
   display: none;
 }
-
-#you,
-#submit {
-    position: absolute;
-    width:100%;
-    min-height: 100vh;
-    display:none;
-    z-index: 100;
-}
-#you img {width: 7.5vw;}
-
-@media print{
-#index,
-#greeting,
-#ver #grid,
-#ver #searchBox
-{display:none;}
-
-#you,
-#submit
-{display:block;}
-
-#submit {
-    top:100vh;
-    background:#fff;
-}
-#you h1 {
-    bottom:0; left:0;
-    width:95%;
-    padding:0 2.5%;
-    font-size:4.5vw;
-    position: fixed;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap:wrap;
-    font-family: "ipag", monospace;
-}
-#you h1 b {
-    max-width:33.5%;
-    text-align:right;
-    word-break: break-word;
-    
-}
-#submit h1 {
-    top:0; left:0;
-    padding:25% 7.5% 12.5%;
-    line-height:150%;
-    font-size:4.5vw;
-    font-family: "ipag", monospace;
-}
-#submit p {
-    top:0; left:0;
-    padding:12.5% 7.5%;
-    line-height:150%;
-    font-size:2.5vw;
-    font-family: "ipag", monospace;
-}
-#you h1 b,
-#you h1 span,
-#submit h1,
-#submit p {
-    display: inline-block;
-    transform:scale(1, 2);
-}
-}
 </style>
 </head>
 <body>
 <div id="index"></div>
 <div id="greeting"></div>
 <div id="ver"></div>
-
-<div id="you">
-<h1><span>Drawing by</span>
-<img src="qr.png">
-<span><?php echo $_SERVER['REMOTE_ADDR']; ?></span></h1>
-</div>
-<div id="submit">
-<h1>OMG!<br/>
-Your Drawing is Seems So Beautiful<3<br/>
-Print it to PDF
-and Send it to us !!<br/>
-<a href="mailto:pehu@creative-community.space">pehu@creative-community.space</a> *
-</h1>
-<p>
-This Email address is for receive-only.<br/>
-We will reply from other addresses.<br/>
-Thank You,<br/>
-<br/>
-creative-community.space
-</p>
-</div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
