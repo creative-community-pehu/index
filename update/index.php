86adf54eed48d1ce465cdf58eae5dd23d2728d17
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-07 20:23:41 +0900

    Update index.php

--- update/index.php
+++ update/index.php
@@ -61,7 +61,7 @@ $(function(){
   grid-row-start: 3;
   grid-row-end: 5;
 }
-#grid div:nth-of-type(7) {
+#grid div:nth-of-type(8) {
   grid-column-start: 2;
   grid-row-start: 5;
   grid-row-end: 7;
