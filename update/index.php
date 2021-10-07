c6bdd87c5b1d7f551db986520145ecd57a03ebfe
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-08 00:12:20 +0900

    Update index.php

--- update/index.php
+++ update/index.php
@@ -184,7 +184,7 @@ li {list-style: none;}
   </form>
 
 <div id="grid">
-<div class="list_item list_toggle" data-type="">
+<div class="list_item list_toggle" data-type="new upgrade tba">
 <p><i>Update | 更新情報一覧</i></p>
 <span>
 <u style="display:inline;">View All | 全部見る</u>
