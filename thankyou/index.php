6fd7c6e66682c617bdc1b0ce0e3b988b6ca055bd
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-07 21:29:38 +0900

    Create index.php

---
+++ thankyou/index.php
@@ -0,0 +1,48 @@
+<?php
+
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+$link = (string)filter_input(INPUT_POST, 'link'); // $_POST['link']
+
+$fp = fopen('all.csv', 'a+b');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    flock($fp, LOCK_EX);
+    fputcsv($fp, [$symbol, $color]);
+    rewind($fp);
+}
+flock($fp, LOCK_SH);
+while ($row = fgetcsv($fp)) {
+    $rows[] = $row;
+}
+flock($fp, LOCK_UN);
+fclose($fp);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Everyones Drawings are Seems So Beautiful</title>
+    <style type="text/css">
+    </style>
+    
+    <!-- Import the webpage's stylesheet -->
+    <link rel="stylesheet" href="/members/sign/all/index.css" />
+  
+  </head>
+  <body>
+    <ul class="mousedragscrollable library">
+    <li>Greeting</li>
+      <?php if (!empty($rows)): ?>
+        <?php foreach ($rows as $row): ?>
+          <li><iframe src="<?=h($row[0])?>"></iframe></li>
+          <?php endforeach; ?>
+        <?php else: ?>
+      <?php endif; ?>
+    </ul>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="/coding/js/mousedragscrollable/scrollable.js"></script>
+  </body>
+</html>
