1a1046821fd44e575fba0ef2b688b24745be8eaf
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-08 02:46:15 +0900

    Update index.php

--- ver/index.php
+++ ver/index.php
@@ -100,7 +100,7 @@ fclose($fp);
 }
 #grid span {
   display: inline-block;
-  padding: 1vw 0.25vw;
+  padding: 1.5vw 0.25vw;
   font-size: 55%;
   position: absolute; z-index:2;
   bottom:0; right:0;
