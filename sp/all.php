6453606bb4478b8a259a5e046c96dc79c2950cc1
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-08 20:51:00 +0900

    Update all.php

--- sp/all.php
+++ sp/all.php
@@ -89,12 +89,12 @@ fclose($fp);
 <li class="must positive">色</li>
 <li class="neither">と</li>
 <li class="must positive">記号</li>
-<li class="">は、</li>
+<li class="">は、この</li>
 <li class="neither">展覧会</li>
 <li class="must positive">新しい生活を集める</li>
-<li class="">のウェブサイトへ</li>
+<li class="">の特設ウェブサイト内に</li>
 <li class="unknown can">2021年9月9日から10月9日</li>
-<li class="">の間に訪れてくださった皆様が投稿した</li>
+<li class="">の期間のみ限定設置した投稿フォームに気づいてくださった皆様が投稿した</li>
 <li class="must unknown">自分の気持ちを知る・表す</li>
 <li class="must positive">色</li>
 <li class="neither">と</li>
