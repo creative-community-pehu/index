5565dfce6572bde820756b782d4d66fc3271bb50
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-09 17:25:21 +0900

    Update index.php

--- ver/index.php
+++ ver/index.php
@@ -26,6 +26,7 @@ fclose($fp);
 <html>
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="icon" href="/logo.png">
 <link rel="stylesheet" href="/coding/fontbook/css/font-family.css"/>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="viewport" content="width=device-width">
