8e0b558cf5bf40e9319bb8e6a09dca0b500835d5
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-11 10:32:22 +0900

    Create showTime.js

---
+++ showTime.js
@@ -0,0 +1,16 @@
+function set2(num) {
+  // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+  let ret;
+  if (num < 10) { ret = "0" + num; }
+  else { ret = num; }
+  return ret;
+}
+function showClock() {
+  const nowTime = new Date();
+  const nowHour = set2(nowTime.getHours());
+  const nowMin = set2(nowTime.getMinutes());
+  const nowSec = set2(nowTime.getSeconds());
+  const msg = "" + nowHour + ":" + nowMin + ":" + nowSec + " Osaka, Japan";
+  document.getElementById("showTime").innerHTML = msg;
+}
+setInterval('showClock()', 1000);
