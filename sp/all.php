a4668863bf8bd4e98d15c62329ea02ef6f9ff592
∧° ┐ <we.are.pe.hu@gmail.com>
2021-10-31 11:10:24 +0900

    Update all.php

--- sp/all.php
+++ sp/all.php
@@ -35,6 +35,56 @@ fclose($fp);
 <link rel="stylesheet" type="text/css" href="https://creative-community.space/sign/stylesheet.css" />
 <title>自分の気持ちを知る・表す</title>
 <style type="text/css">
+#log {
+  font-size:4vw;
+  width:45%;
+  height:75vh;
+  margin:12.5vh auto;
+  background:rgba(255,255,255,0.75);
+  overflow-y:auto;
+}
+#log_items {
+  padding:0 2.5vw;
+  list-style: none;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-align-items: flex-start;
+  align-items: flex-start;
+  -webkit-flex-direction: column-reverse;
+  flex-direction: column-reverse;
+}
+#log_items li {
+  width:100%;
+  position:relative;
+  margin:0;
+  padding:2.5vw 0 0;
+  word-break: break-word;
+}
+#log_items p {
+  margin:0;
+  font-size:75%;
+  line-height:150%;
+}
+#log_items u {
+  display:inline-block;
+  width:4vw;
+  height:4vw;
+  position:relative;
+}
+#log_items span {
+  position:absolute;
+  padding:0;
+  margin:0;
+  top:50%; left:50%;
+  transform:translate(-50%,-50%);
+  -webkit-transform:translate(-50%,-50%);
+}
+#log_items b {
+  font-size:150%;
+}
+#log_items i {
+  font-size:125%;
+}
 #marquee {
 	position:relative;
 	z-index:10;
@@ -80,6 +130,28 @@ fclose($fp);
 <span class="bg_symbol">✔</span>
 </li>
 <?php endif; ?>
+<li style="background:#fff;">
+<div id="log">
+<ul id="log_items">
+<?php if (!empty($rows)): ?>
+<?php foreach ($rows as $row): ?>
+<li>
+<p><u style="background:#<?=h($row[1])?>;"><span><?=h($row[0])?></span></u> IP <b style="color:#<?=h($row[1])?>; filter: invert();"><?=h($row[3])?></b><br/>
+Posted on <i style="color:#<?=h($row[1])?>;"><?=h($row[2])?></i></p>
+</li>
+<?php endforeach; ?>
+<?php else: ?>
+<li>
+<p>IP <b>000.00.00.00</b><br/>
+Posted on <i>00.00.00 00:00</i></p>
+</li>
+<?php endif; ?>
+<li>
+Log
+</li>
+</ul>
+</div>
+</li>
 <li style="background:#fff;">
 <div id="marquee">
 <div class="outside org_list">
