0b24446aac058006e45537c099602a4f8f447c80
∧° ┐ <we.are.pe.hu@gmail.com>
2021-10-31 21:33:47 +0900

    Update gradation.php

--- sp/gradation.php
+++ sp/gradation.php
@@ -90,7 +90,7 @@ body  {overflow-x:hidden;}
 
 <ul id="symbol_color">
 <li class="bg_color" style="background:#fff;">
-<span class="bg_symbol">制作中</span>
+<span class="bg_symbol">準備中</span>
 </li>
 </ul>
 
