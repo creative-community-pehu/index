d3cda8a69f558236d160a7cc9a9b4a1c2914a6a9
pe-hu <we.are.pe.hu@gmail.com>
2021-10-31 15:27:58 +0900

    Update all.php

--- sp/all.php
+++ sp/all.php
@@ -123,7 +123,6 @@ fclose($fp);
 <script type="text/javascript">
 var day = '<div id="day"></div>';
 var night = '<div id="night"></div>';
-var close = '<div id="close"></div>';
 
 var now = new Date();
 var hour = now.getHours();
