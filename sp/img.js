47a111aefab14f290eb7268cab7b33c4e90ffce2
pe-hu <we.are.pe.hu@gmail.com>
2021-11-03 16:35:49 +0900

    Update img.js

--- sp/img.js
+++ sp/img.js
@@ -1,23 +1,2 @@
-const random = [
-  "print/drawings.JPG",
-  "print/org.jpg",
-  "print/orgup.jpg",
-  "print/sign.JPG",
-  "print/weareopen.JPG",
-  "print/weareclose.JPG"
-  ];
-  
-  function randomImg(randomArray) {
-    var random =
-    randomArray[Math.floor(Math.random() * randomArray.length)];
-    console.log(random);
-    return random;
-  }
-  function imgGenerator() {
-    var img = `<img src="${randomImg(random)}">`;
-    document.querySelector(".random").innerHTML = img;
-  }
-  window.setInterval(function() {
-    imgGenerator();
-  }, 2000);
+
   
