3c072bbbf07e0c1eda43f46eae03f066ba23217f
∧° ┐ <we.are.pe.hu@gmail.com>
2022-03-19 02:04:02 +0900

    Update index.php

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ body {margin:0; padding:0;}
 
 #mobile h1 {
     top:0; left:0;
-    padding:5.5rem 7.5%;
+    padding:2.5rem 7.5% 5.5rem;
     line-height:150%;
     font-size:1.5rem;
     font-family: "ipag", monospace;
