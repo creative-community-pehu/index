4f69dc8caffad92f512e428d3b033d4674407b98
∧° ┐ <we.are.pe.hu@gmail.com>
2022-03-27 22:56:27 +0900

    now

--- ver/index.php
+++ ver/index.php
@@ -236,23 +236,6 @@ $(function() {
   });
 });
 
-$(document).on('mousemove', function(e){
-  var hueraw = parseInt(255 - Math.round((e.pageY + 0.1) / ($(window).height()) * 255));
-  var hue = '"srff"' + hueraw;
-
-    $('#huecount').text(hueraw);
-    $('#lightnesscount').text(hueraw + '%');
-    $('#saturationcount').text(hueraw + '%');
-
-    if((e.pageX <= $(window).width()/1)){
-    var sraw = parseInt(100 - Math.round((e.pageX + 0.1) / ($(window).width()) * 100));
-      var lraw = parseInt(Math.round((e.pageX + 0.1) / ($(window).width()) * 100));
-      $('#color').css({'background': 'hsl(' + hueraw + ',' + sraw + '%,' + lraw + '%)'})
-      $('#saturationcount').text(sraw + '%');
-      $('#lightnesscount').text(lraw + '%');
-  }
-});
-
 $('a[href^="#"]').click(function(){
    var speed = 500;　//スクロールスピード
    var href= $(this).attr("href");
