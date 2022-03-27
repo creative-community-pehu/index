c9adbea0aed86e23f02627ed3ef4cabee9201b4c
∧° ┐ <we.are.pe.hu@gmail.com>
2022-03-27 22:42:51 +0900

    index

--- index.php
+++ index.php
@@ -150,7 +150,7 @@ body {margin:0; padding:0;}
 }
 </style>
 </head>
-<body>
+<body id="color">
 <div id="greeting"></div>
 <div id="ver"></div>
 <div id="now">
@@ -195,6 +195,23 @@ $('a[href^="#"]').click(function(){
    $("html, body").animate({scrollTop:position}, speed, "swing");
    return false;
  });
+
+$(document).on('mousemove', function(e){
+  var hueraw = parseInt(255 - Math.round((e.pageY + 0.1) / ($(window).height()) * 255));
+  var hue = '"srff"' + hueraw;
+
+    $('#huecount').text(hueraw);
+    $('#lightnesscount').text(hueraw + '%');
+    $('#saturationcount').text(hueraw + '%');
+
+    if((e.pageX <= $(window).width()/1)){
+    var sraw = parseInt(100 - Math.round((e.pageX + 0.1) / ($(window).width()) * 100));
+      var lraw = parseInt(Math.round((e.pageX + 0.1) / ($(window).width()) * 100));
+      $('#color').css({'background': 'hsl(' + hueraw + ',' + sraw + '%,' + lraw + '%)'})
+      $('#saturationcount').text(sraw + '%');
+      $('#lightnesscount').text(lraw + '%');
+  }
+});
 
 var COLOURS = [ '#EEE' ];
 var radius = 0;
