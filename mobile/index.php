b4d45d3043c25e5c5bc840ccd8e995ab503914ab
∧° ┐ <we.are.pe.hu@gmail.com>
2022-06-25 00:55:30 +0900

    Update index.php

--- mobile/index.php
+++ mobile/index.php
@@ -148,6 +148,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="../now.js"></script>
     <script type="text/javascript">
         $(function() {
             $("#greeting").load("../hello.php");
