b2eb29b80d0e1bed75142c29bf53c773a4aec88d
∧° ┐ <we.are.pe.hu@gmail.com>
2022-06-25 02:28:04 +0900

    Update mobile.php

--- mobile.php
+++ mobile.php
@@ -108,7 +108,7 @@
             background: #fff;
             border: 1px solid #000;
             border-radius: 50%;
-            padding: 0.5vw 1.25vw;
+            padding: 0.5vw 2vw;
             top: 75%;
             font-size: 2.5vw;
             font-family: "DiaPro";
