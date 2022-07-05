43c589ffa010e9d6045815f45406991b7302ed27
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-05 23:07:10 +0900

    Update log.php

--- bnaaltermuseum/log.php
+++ bnaaltermuseum/log.php
@@ -117,10 +117,12 @@ fclose($fp);
 </li>
 <?php endif; ?>
 <li>
+<i>
 <?php
 date_default_timezone_set('Asia/Tokyo');
 print(date('Y年n月j日'))
 ?>
+</i>
 の<br/>
 気持ちを表す <b>色と記号</b>
 <p><br/>This is The Colors and Symbols That Suits on 
