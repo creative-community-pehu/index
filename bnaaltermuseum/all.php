23afb050c5fc0f918f5bb80858aeb4545494a89d
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-06 03:23:02 +0900

    bna

--- bnaaltermuseum/all.php
+++ bnaaltermuseum/all.php
@@ -30,31 +30,80 @@ flock($fp, LOCK_UN);
 fclose($fp);
 
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" type="text/css" href="stylesheet.css" />
-<title>自分の気持ちを知る・表す</title>
-<style type="text/css">
-</style>
-</head>
-<body>
-  
-<ul id="symbol_color">
-<?php if (!empty($rows)): ?>
-<?php foreach ($rows as $row): ?>
-<li class="bg_color" style="background:#<?=h($row[1])?>;">
-<span class="bg_symbol" style="color:#<?=h($row[1])?>; filter: invert();"><?=h($row[0])?></span>
-</li>
-<?php endforeach; ?>
-<?php else: ?>
-<li class="bg_color" style="background:#fff;">
-<span class="bg_symbol">✔</span>
-</li>
-<?php endif; ?>
-</ul>
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="ja">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>自分の気持ちを知る・表す</title>
+        <style type="text/css">
+            body {
+                padding: 0;
+                margin: 0;
+            }
+            
+            li {
+                list-style: none;
+            }
+            
+            #symbol_color {
+                top: 0;
+                left: 0;
+                padding: 0;
+                margin: 0;
+                width: 100%;
+                position: relative;
+                z-index: 0;
+                overflow-y: auto;
+                overflow-x: hidden;
+                display: flex;
+                flex-direction: column-reverse;
+            }
+            
+            #symbol_color .bg_color {
+                position: relative;
+                top: 0;
+                left: 0;
+                padding: 0;
+                margin: 0;
+                width: 100%;
+                height: 100vh;
+                display: block;
+            }
+            
+            #symbol_color .bg_symbol {
+                font-size: 15vw;
+                position: absolute;
+                padding: 0;
+                margin: 0;
+                top: 50%;
+                left: 50%;
+                z-index: 10;
+                display: flex;
+                flex-wrap: wrap;
+                transform: translate(-50%, -50%);
+                -webkit-transform: translate(-50%, -50%);
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <ul id="symbol_color">
+            <?php if (!empty($rows)): ?>
+            <?php foreach ($rows as $row): ?>
+            <li class="bg_color" style="background:#<?=h($row[1])?>;">
+                <span class="bg_symbol" style="color:#<?=h($row[1])?>; filter: invert();"><?=h($row[0])?></span>
+            </li>
+            <?php endforeach; ?>
+            <?php else: ?>
+            <li class="bg_color" style="background:#fff;">
+                <span class="bg_symbol">✔</span>
+            </li>
+            <?php endif; ?>
+        </ul>
+    </body>
+
+    </html>
--- bnaaltermuseum/menu/menu.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.change').hide();
-  
-    $('.tab').on('click',function(){
-      $('.change').not($($(this).attr('href'))).hide();
-      $($(this).attr('href')).fadeToggle(1000);
-    });
-  });
