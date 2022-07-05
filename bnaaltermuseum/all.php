3960aee6d905a156ebde3313bd96d6ace727cbda
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-05 23:22:19 +0900

    bna

--- bnaaltermuseum/all.php
+++ bnaaltermuseum/all.php
@@ -6,11 +6,13 @@ function h($str) {
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
-$today = date("Ymd");
+$year = date("Y");
+$month = date("m");
+$day = date("d");
 $symbol = (string)filter_input(INPUT_POST, 'symbol'); // $_POST['symbol']
 $color = (string)filter_input(INPUT_POST, 'color'); // $_POST['color']
 $timestamp = date("g:i:s A \J\S\T");
-$filename =  $today . ".csv"; 
+$filename =  $year . "/" . $month . "/" . $day . ".csv"; 
 
 $forwardedFor = $_SERVER["REMOTE_ADDR"];
 $ips = explode(",", $forwardedFor);
