5e527736fabdc2e43303e25dc2be3b74eb0878a1
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-06 15:59:58 +0900

    Update log.php

--- bnaaltermuseum/sign/log.php
+++ bnaaltermuseum/sign/log.php
@@ -33,6 +33,11 @@ fclose($fp);
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <title>自分の気持ちを知る・表す</title>
         <style type="text/css">
+.SFCompact {font-family: "SF Compact";}
+@font-face {
+  font-family: "SF Compact";
+  src: url("https://creative-community.space/coding/fontbook/family/SF-Compact-Text-Regular.otf");
+}
             body {
                 padding: 0;
                 margin: 0;
@@ -49,7 +54,7 @@ fclose($fp);
                 margin: 10vh 25% 0;
                 z-index: 1000;
                 overflow-y: auto;
-                font-family: "MS Mincho", "SimSong", serif;
+                font-family: "Helvetica Neue" , "SF Compact", Helvetica, "YuGothic" , sans-serif;
                 font-weight: 500;
             }
             
