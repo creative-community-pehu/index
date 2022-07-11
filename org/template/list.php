0231ddf8722307904e8b7140d3d51bbff109b90a
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-12 06:41:34 +0900

    Update list.php

--- org/template/list.php
+++ org/template/list.php
@@ -36,7 +36,7 @@ fclose($fp);
         <style>
         h3 {
             font-size:1rem;
-            margin: 0.5rem;
+            margin: 0.5rem    0.25rem;
             font-weight: 500;
             display: inline-block;
             font-family: "Arial Narrow",monospace;
