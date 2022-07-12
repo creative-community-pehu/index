7c4cb405f8dbef43f4776d07da4a35db0ae73b04
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-12 12:29:01 +0900

    Update motto.php

--- org/motto.php
+++ org/motto.php
@@ -41,8 +41,8 @@ fclose($fp);
 
         h3 {
             font-family: "MS Mincho", serif;
-            font-size:1.25rem;
-            margin: 1rem 0.5rem;
+            font-size:1.5rem;
+            margin: 0.75rem 0.5rem;
             font-weight: 500;
             transition: all 1000ms ease;
         }
