d27097a6056ad70db7751bc1c75ec7aa981cfdad
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-18 00:03:49 +0900

    Update reception.php

--- bnaaltermuseum/reception.php
+++ bnaaltermuseum/reception.php
@@ -164,7 +164,7 @@
     
     <div id="weather">
         <span>今日の天気</span>
-        <marquee>aaa</marquee>
+        <marquee></marquee>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
